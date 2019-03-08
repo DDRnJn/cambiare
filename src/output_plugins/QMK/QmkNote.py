19b2a43aba5bef638039c3e15b829b5526ec3232
DDRnJn <dhruv.ranjan@gmail.com>
2019-03-08 15:34:11 -0500

    Adjustment to note duration for QMK
    Adjusted note durations for the QMK exporter, since the default tempo in QMK is too fast
    The new qmk_duration_mapping_slow dict maps each note to the equivilant note with twice the duration
    It should be noted (heh) that NoteType.whole and NoteType.dotted_whole now map to B__NOTE and BD__NOTE respectively
    These are meant to represent Breves and Dotted Breves. These aren't currently added to the musical_notes.h header in QMK, so these will throw an error. I'm planning to add them to QMK at some point. As a temporary fix for these, after the output file is created, they can be converted to M__NOTE(note_name, 128) and M__NOTE(note_name, 192) respectively

--- src/output_plugins/QMK/QmkNote.py
+++ src/output_plugins/QMK/QmkNote.py
@@ -18,13 +18,30 @@ class QmkNote:
             NoteType.dotted_sixteenth: "SD_NOTE",  # Dotted Sixteenth Note
         }
 
+        #  Added because the default tempo in QMK is 100, which is way too fast
+        #  So this dict instead maps to double the actual note value
+        #  Except for whole notes, since there's no double whole/breve type in QMK
+        #  Maybe I'll add that later
+        self.qmk_duration_mapping_slow = {
+            NoteType.whole: "B__NOTE",  # Whole Note
+            NoteType.half: "W__NOTE",  # Half Note
+            NoteType.quarter: "H__NOTE",  # Quarter Note
+            NoteType.eighth: "Q__NOTE",  # Eighth Note
+            NoteType.sixteenth: "E__NOTE",  # Sixteenth Note
+            NoteType.dotted_whole: "BD_NOTE",  # Dotted Whole Note
+            NoteType.dotted_half: "WD_NOTE",  # Dotted Half Note
+            NoteType.dotted_quarter: "HD_NOTE",  # Dotted Quarter Note
+            NoteType.dotted_eighth: "QD_NOTE",  # Dotted Eighth Note
+            NoteType.dotted_sixteenth: "ED_NOTE",  # Dotted Sixteenth Note
+        }
+
         self.note_type = self.parse_note_type(note_type)
         self.note_pitch = self.parse_note_pitch(note_pitch)
         #self.qmk_string = self.parse_to_qmk_string(music21_note)
 
     def parse_note_type(self, note_type):
-        if note_type in self.qmk_duration_mapping:
-            return self.qmk_duration_mapping[note_type]
+        if note_type in self.qmk_duration_mapping_slow:
+            return self.qmk_duration_mapping_slow[note_type]
         else:
             return NoteType.quarter
 
